28eb025e8b5d48a50bcef982ce0e7815b8d8825e
Natthaphol Nirattisaikul <zerozkiller@gmail.com>
2024-04-22 01:26:09 +0700

    update task.sql

--- tasks.sql
+++ tasks.sql
@@ -7,10 +7,9 @@ CREATE TABLE tasks (
     taskTitle VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
     taskDescription VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
     taskAssignees VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-    taskStatus ENUM('No Status', 'To Do', 'Doing', 'Done') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'No Status',
+    taskStatus ENUM('No Status', 'To Do', 'Doing', 'Done') NOT NULL DEFAULT 'No Status',
     createdOn DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updatedOn DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    CHECK (LENGTH(taskTitle) > 0),
-    CHECK (LENGTH(taskDescription) > 0 OR taskDescription IS NULL),
-    CHECK (LENGTH(taskAssignees) > 0 OR taskAssignees IS NULL)
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    CONSTRAINT CHK_taskTitle_not_empty CHECK (taskTitle <> ''),
+    CONSTRAINT CHK_taskAssignees_not_empty CHECK (taskAssignees <> '')
+);
