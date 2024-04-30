718eaa2673a175cd206a306696fa2f50c9eb7f5d
Niraphan <niraphan2560@gmail.com>
2024-04-30 23:23:37 +0700

    Updating delete

--- integrated-1-frontend/src/components/confirmDelete.vue
+++ integrated-1-frontend/src/components/confirmDelete.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { defineProps, defineEmits, onUnmounted } from 'vue'
 import { deleteTask } from '../libs/FetchAPI.js'
+import { useTasks } from '@/stores/task';
+
 
 const props = defineProps({
   task: {
@@ -8,12 +10,17 @@ const props = defineProps({
   },
 })
 
+
+const myTasks = useTasks()
+
 const emit = defineEmits(['closeModal']) 
 
 const submitDelete = async () => {
   // Call deleteTask function with taskId
-  await deleteTask(props.task.id)
+  const result = await deleteTask(props.task.id)
+  myTasks.removeTask(result.id)
   emit('closeModal')
+  
 }
 
 const cancelDelete = () => {
@@ -26,12 +33,11 @@ const cancelDelete = () => {
 
 
 <template>
-  <div class="py-[10vh] px-[10vh] fixed inset-0 flex justify-center bg-black bg-opacity-35 w-full">
-    <div class="bg-white flex-col border rounded-md p-[3vh] w-[100vh]">
+  <div class="py-[30vh] px-[10vh] fixed inset-0 flex justify-center bg-black bg-opacity-35 w-full z-50">
+    <div class="bg-white  flex-col border rounded-md p-[2vh] w-fit h-fit">
       <p class="font-bold text-[4vh] py-[2vh]">Delete a Task</p>
       <hr />
-      <p class="py-[3vh]">Do you want to delete the task "{{ task.title}}"?</p>
-      <hr />
+      <p class="py-[3vh]">Do you want to delete the task "{{ task.title }}" ?</p>
       <div class="flex gap-[2vh] justify-end py-[2vh]">
         <button @click="submitDelete" class="bg-red-500 text-white rounded-md p-2">Delete</button>
         <button @click="cancelDelete" class="bg-blue-500 text-white rounded-md p-2">Cancel</button>
