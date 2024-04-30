e51de524712bfdad8c255b83b6ec8aa9e93738ed
yadajen <yada2342547@gmail.com>
2024-04-30 12:02:34 +0700

    update last version

--- integrated-1-frontend/src/stores/task.js
+++ integrated-1-frontend/src/stores/task.js
@@ -2,54 +2,58 @@ import { defineStore, acceptHMRUpdate } from 'pinia'
 import { ref } from 'vue'
 
 const useTasks = defineStore('tasks', () => {
-    const tasks = ref([])
+  const tasks = ref([])
 
-    function addTasks(newTasks) {
-        newTasks.forEach((newTask) => addTask(newTask))
-    }
+  function addTasks(newTasks) {
+    newTasks.forEach((newTask) => addTask(newTask))
+  }
 
-    function addTask(task) {
-        tasks.value.push(task)
-    }
+  function addTask(task) {
+    task.status = task.status
+      .split('_')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ')
+    tasks.value.push(task)
+  }
 
-    function updateTask(updatedTask) {
-        const index = findIndexTask(updatedTask.id)
-        if (index !== -1) {
-            tasks.value.splice(index, 1, updatedTask)
-        }
+  function updateTask(updatedTask) {
+    const index = findIndexTask(updatedTask.id)
+    if (index !== -1) {
+      tasks.value.splice(index, 1, updatedTask)
     }
+  }
 
-    function getIdOfTask(searchId) {
-        return tasks.value.find((task) => task.id == searchId)
-    }
+  function getIdOfTask(searchId) {
+    return tasks.value.find((task) => task.id == searchId)
+  }
 
-    function findIndexTask(searchId) {
-        return tasks.value.findIndex((task) => task.id == searchId)
-    }
+  function findIndexTask(searchId) {
+    return tasks.value.findIndex((task) => task.id == searchId)
+  }
 
-    function removeTask(removeId) {
-        const index = findIndexTask(removeId)
-        if (index !== -1) {
-            tasks.value.splice(index, 1)
-        }
+  function removeTask(removeId) {
+    const index = findIndexTask(removeId)
+    if (index !== -1) {
+      tasks.value.splice(index, 1)
     }
+  }
 
-    function getTasks() {
-        return tasks.value
-    }
+  function getTasks() {
+    return tasks.value
+  }
 
-    return {
-        addTasks,
-        updateTask,
-        getIdOfTask,
-        findIndexTask,
-        removeTask,
-        getTasks,
-    }
+  return {
+    addTasks,
+    updateTask,
+    getIdOfTask,
+    findIndexTask,
+    removeTask,
+    getTasks,
+  }
 })
 
 export { useTasks }
 
 if (import.meta.hot) {
-    import.meta.hot.accept(acceptHMRUpdate(useTasks, import.meta.hot))
+  import.meta.hot.accept(acceptHMRUpdate(useTasks, import.meta.hot))
 }
