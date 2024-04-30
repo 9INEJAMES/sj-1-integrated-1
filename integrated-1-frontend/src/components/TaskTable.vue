ed2ae110a994510fdd0181604173b532885d4c73
Niraphan <niraphan2560@gmail.com>
2024-04-30 15:57:47 +0700

    Updating some details

--- integrated-1-frontend/src/components/TaskTable.vue
+++ integrated-1-frontend/src/components/TaskTable.vue
@@ -10,7 +10,6 @@ const props = defineProps({
 })
 const emit = defineEmits(["getTask"])
 const getTask = (id) => {
-<<<<<<< HEAD
     emit("getTask", id)
 }
 const colorStatus = (task) => {
@@ -23,20 +22,6 @@ const colorStatus = (task) => {
     } else {
         return "bg-amber-300"
     }
-=======
-  emit('getTask', id)
-}
-const colorStatus = (task) => {
-  if (task == 'Done') {
-    return 'bg-emerald-500'
-  } else if (task == 'Doing') {
-    return 'bg-sky-300'
-  } else if (task == 'No Status') {
-    return 'bg-slate-300'
-  } else {
-    return 'bg-amber-300'
-  }
->>>>>>> cdb791423cedad91feb1810d82e410aef0081b48
 }
 
 const toEditPage = (id) => {
@@ -55,75 +40,55 @@ const deleteTask = (task) => {
 </script>
 
 <template>
-<<<<<<< HEAD
-    <tr v-for="(task, index) in taskList" :key="task.id" class="itbkk-item">
-        <!-- Pass selectedTask to ConfirmDelete component -->
-        <ConfirmDelete v-if="deleteModal" :task="selectedTask" @closeModal="deleteModal = false" />
-        <td>
-            <div class="flex">
-            {{ index + 1 }}
-            <div class="dropdown dropdown-hover">
-                <div tabindex="0" role="button" ><img src="/element/dots.png" class=" w-[2vh] h-[2vh]" alt="list img"></div>
-                <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+    <div class="overflow-x-auto">
+      <table class="myTable">
+        <!-- head -->
+        <thead>
+          <tr>
+            <th></th>
+            <th>Title</th>
+            <th>Assignees</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody> <!-- Add tbody here -->
+          <tr v-for="(task, index) in taskList" :key="task.id" class="itbkk-item">
+            <!-- Pass selectedTask to ConfirmDelete component -->
+            <ConfirmDelete v-if="deleteModal" :task="selectedTask" @closeModal="deleteModal = false" />
+            <td>
+              <div class="flex">
+                {{ index + 1 }}
+                <div class="dropdown dropdown-hover">
+                  <div tabindex="0" role="button">
+                    <img src="/element/dots.png" class=" w-[2vh] h-[2vh]" alt="list img">
+                  </div>
+                  <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
                     <li><p @click="">edit</p></li>
                     <!-- Pass the task to deleteTask method when delete is clicked -->
                     <li><p @click="deleteTask(task)">delete</p></li>
-                </ul>
-            </div>
-        </div>
-        </td>
-        <td class="itbkk-title">
-            <p class="font-bold pb-[2vh] hover:text-blue-500 break-all hover:cursor-pointer" @click="getTask(task.id)">
+                  </ul>
+                </div>
+              </div>
+            </td>
+            <td class="itbkk-title">
+              <p class="font-bold pb-[2vh] hover:text-blue-500 break-all hover:cursor-pointer" @click="getTask(task.id)">
                 {{ task.title }}
-            </p>
-        </td>
-        <td class="itbkk-assignees" :class="task.assignees ? '' : 'italic text-gray-500'">
-            {{ task.assignees ? task.assignees : "Unassigned" }}
-        </td>
-        <td class="itbkk-status">
-            <button class="rounded-2xl w-[100px] h-[30px] text-[2vh] font-bold cursor-default"
-                :class="colorStatus(task.status)">
-                {{ task.status }}
-=======
-  <div class="overflow-x-auto">
-    <table class="myTable">
-      <!-- head -->
-      <thead>
-        <tr class="text-lg bg-black text-amber-50">
-          <th></th>
-          <th>Title</th>
-          <th>Assignees</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(task, index) in taskList" :key="task.id" class="itbkk-item">
-          <td>{{ index + 1 }}</td>
-          <td
-            class="itbkk-title font-bold h-[30px] text-[2vh] hover:text-blue-500 break-all hover:cursor-pointer"
-            @click="getTask(task.id)"
-          >
-            {{ task.title }}
-          </td>
-          <td
-            class="itbkk-assignees"
-            :class="task.assignees ? '' : 'italic text-gray-500'"
-          >
-            {{ task.assignees ? task.assignees : 'Unassigned' }}
-          </td>
-          <td class="itbkk-status">
-            <button
-              class="rounded-2xl w-[100px] h-[30px] text-[2vh] font-bold cursor-default"
-              :class="colorStatus(task.status)"
-            >
-              {{ task.status }}
->>>>>>> cdb791423cedad91feb1810d82e410aef0081b48
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</template>
+              </p>
+            </td>
+            <td class="itbkk-assignees" :class="task.assignees ? '' : 'italic text-gray-500'">
+              {{ task.assignees ? task.assignees : "Unassigned" }}
+            </td>
+            <td class="itbkk-status">
+              <button class="rounded-2xl w-[100px] h-[30px] text-[2vh] font-bold cursor-default"
+                  :class="colorStatus(task.status)">
+                  {{ task.status }}
+              </button>
+            </td>
+          </tr>
+        </tbody> <!-- Close tbody here -->
+      </table>
+    </div>
+  </template>
+  
 
 <style scoped></style>
