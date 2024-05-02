6eb25b9d6103557766d2745425153e087f4fe3d9
yadajen <yada2342547@gmail.com>
2024-05-02 14:46:50 +0700

    Update TaskAddPage.vue

--- integrated-1-frontend/src/components/TaskAddPage.vue
+++ integrated-1-frontend/src/components/TaskAddPage.vue
@@ -11,161 +11,207 @@ const router = useRouter()
 const myTasks = useTasksStore()
 const taskApi = useTaskApi()
 const newTask = ref({
-    title: '',
-    description: '',
-    assignees: '',
-    status: 'No Status',
+  title: '',
+  description: '',
+  assignees: '',
+  status: 'No Status',
 })
 const isDisibled = ref(false)
 const localTimeZone = ref('')
 const createdOn = ref('')
 const updatedOn = ref('')
 const submitTask = async (isSave) => {
-    if (isSave) {
-        if (route.params.taskId) {
-            const updated = await taskApi.updateTask(newTask.value)
-            myTasks.updateTask({
-                ...updated,
-                title: newTask.value.title,
-                description: !newTask.value.description ? (newTask.value.description = '') : newTask.value.description,
-                assignees: !newTask.value.assignees ? (newTask.value.assignees = '') : newTask.value.assignees,
-                status: newTask.value.status,
-            })
-        } else {
-            const task = await taskApi.addTask(newTask.value)
-            myTasks.addTasks([task])
-        }
+  if (isSave) {
+    if (route.params.taskId) {
+      const updated = await taskApi.updateTask(newTask.value)
+      myTasks.updateTask({
+        ...updated,
+        title: newTask.value.title,
+        description: !newTask.value.description
+          ? (newTask.value.description = '')
+          : newTask.value.description,
+        assignees: !newTask.value.assignees
+          ? (newTask.value.assignees = '')
+          : newTask.value.assignees,
+        status: newTask.value.status,
+      })
+    } else {
+      const task = await taskApi.addTask(newTask.value)
+      myTasks.addTasks([task])
     }
-    router.push({ path: `/` })
+  }
+  router.push({ path: `/` })
 }
 const checkLength = (name, value, length) => {
-    if (value.trim().length > length) {
-        if (name === 'title') newTask.value.title = value.trim().slice(0, length)
-        if (name === 'description') newTask.value.description = value.trim().slice(0, length)
-        if (name === 'assignees') newTask.value.assignees = value.trim().slice(0, length)
-    }
+  if (value.trim().length > length) {
+    if (name === 'title') newTask.value.title = value.trim().slice(0, length)
+    if (name === 'description')
+      newTask.value.description = value.trim().slice(0, length)
+    if (name === 'assignees')
+      newTask.value.assignees = value.trim().slice(0, length)
+  }
 }
 const switchTimeZone = (task) => {
-    localTimeZone.value = Intl.DateTimeFormat().resolvedOptions().timeZone
-    createdOn.value = new Date(task.createdOn).toLocaleString('en-GB', {
-        timeZone: localTimeZone.value,
-    })
-    updatedOn.value = new Date(task.updatedOn).toLocaleString('en-GB', {
-        timeZone: localTimeZone.value,
-    })
+  localTimeZone.value = Intl.DateTimeFormat().resolvedOptions().timeZone
+  createdOn.value = new Date(task.createdOn).toLocaleString('en-GB', {
+    timeZone: localTimeZone.value,
+  })
+  updatedOn.value = new Date(task.updatedOn).toLocaleString('en-GB', {
+    timeZone: localTimeZone.value,
+  })
 }
 onMounted(async () => {
-    if (route.name === 'taskDetails') {
-        isDisibled.value = true
+  if (route.name === 'taskDetails') {
+    isDisibled.value = true
+  }
+  if (route.name !== 'taskAdd') {
+    const id = route.params.taskId
+    const task = await taskApi.getTaskById(id)
+    if (!task) {
+      setTimeout(() => {
+        router.push({ path: `/` })
+      }, 2000)
+    } else {
+      newTask.value = {
+        id: task.id,
+        title: task.title,
+        description: task.description,
+        assignees: task.assignees,
+        status: task.status
+          .split('_')
+          .map(
+            (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+          )
+          .join(' '),
+        createdOn: task.createdOn,
+        updatedOn: task.updatedOn,
+      }
+      switchTimeZone(task)
     }
-    if (route.name !== 'taskAdd') {
-        const id = route.params.taskId
-        const task = await taskApi.getTaskById(id)
-        if (!task) {
-            setTimeout(() => {
-                router.push({ path: `/` })
-            }, 2000)
-        } else {
-            newTask.value = {
-                id: task.id,
-                title: task.title,
-                description: task.description,
-                assignees: task.assignees,
-                status: task.status
-                    .split('_')
-                    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-                    .join(' '),
-                createdOn: task.createdOn,
-                updatedOn: task.updatedOn,
-            }
-            switchTimeZone(task)
-        }
-    }
+  }
 })
 </script>
 
 <template>
-    <div class="py-[10vh] px-[10vh] fixed inset-0 flex justify-center bg-black bg-opacity-50 w-full">
-        <div class="w-full rounded-lg" :class="myTheme.getTheme()">
-            <div class="grid gap-[2vh] rounded-md border-none p-[2vh]">
-                <p class="text-xl font-semibold">{{ route.params.taskId ? 'Edit' : 'New' }} Task</p>
-                <hr />
-                <div>
-                    <label for="title">Titie</label><br />
-                    <input
-                        type="text"
-                        name="title"
-                        @input="checkLength('title', newTask.title, 100)"
-                        class="itbkk-title block w-full p-[2vh] resize-none text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
-                        id="title"
-                        placeholder="Write your task's title"
-                        v-model="newTask.title"
-                        :disabled="isDisibled"
-                    />
-                </div>
-                <div class="grid grid-cols-12 gap-[3vh]">
-                    <div class="grid col-span-8">
-                        <div>
-                            <label for="description">Description</label>
-                            <textarea
-                                rows="15"
-                                id="description"
-                                class="itbkk-description block w-full p-[2vh] resize-none overflow-auto text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
-                                placeholder="Write your description"
-                                name="description"
-                                @input="checkLength('description', newTask.description, 500)"
-                                v-model="newTask.description"
-                                :disabled="isDisibled"
-                            ></textarea>
-                        </div>
-                    </div>
-
-                    <div class="flex col-span-4 flex-col justify-between">
-                        <div>
-                            <div>
-                                <label for="assignees">Assignees</label>
-                                <textarea
-                                    id="assignees"
-                                    rows="1"
-                                    class="itbkk-assignees block p-[2vh] w-full resize-none text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
-                                    placeholder="Enter assignees"
-                                    name="assignees"
-                                    @input="checkLength('assignees', newTask.assignees, 30)"
-                                    v-model="newTask.assignees"
-                                    :disabled="isDisibled"
-                                ></textarea>
-                            </div>
-                            <div class="flex flex-col pt-[3vh]">
-                                <label for="status">Status</label>
-                                <select id="status" class="itbkk-status select select-bordered disabled:text-white" :class="myTheme.getTheme()" :disabled="isDisibled" v-model="newTask.status">
-                                    <option selected value="No Status">No Status</option>
-                                    <option value="To Do">To Do</option>
-                                    <option value="Doing">Doing</option>
-                                    <option value="Done">Done</option>
-                                </select>
-                            </div>
-                            <div v-if="$route.name != 'taskAdd'" class="pt-[4vh]">
-                                <p class="itbkk-timezone">Local Time Zone: {{ localTimeZone }}</p>
-                                <p class="itbkk-created-on">Created On: {{ createdOn }}</p>
-                                <p class="itbkk-updated-on">Last Updated On: {{ updatedOn }}</p>
-                            </div>
-                        </div>
-                        <div class="pt-[4vh]">
-                            <div v-if="$route.name != 'taskDetails'" class="flex justify-evenly">
-                                <button class="itbkk-button-confirm btn btn-success btn-xs sm:btn-sm md:btn-md lg:btn-lg" @click="submitTask(true)" :disabled="newTask.title.trim().length <= 0">
-                                    Ok
-                                </button>
-                                <button class="itbkk-button-cancel btn btn-error btn-xs sm:btn-sm md:btn-md lg:btn-lg" @click="submitTask(false)">Cancel</button>
-                            </div>
-                            <div v-else class="flex justify-end items-end">
-                                <button class="itbkk-button-close btn btn-error text-white" @click="submitTask(false)">close</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <div
+    class="py-[10vh] px-[10vh] fixed inset-0 flex justify-center bg-black bg-opacity-50 w-full"
+  >
+    <div class="w-full rounded-lg" :class="myTheme.getTheme()">
+      <div class="grid gap-[2vh] rounded-md border-none p-[2vh]">
+        <p class="text-xl font-semibold">
+          {{ route.params.taskId ? 'Edit' : 'New' }} Task
+        </p>
+        <hr />
+        <div>
+          <label for="title">Titie</label><br />
+          <input
+            type="text"
+            name="title"
+            @input="checkLength('title', newTask.title, 100)"
+            class="itbkk-title block w-full p-[2vh] resize-none text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
+            id="title"
+            placeholder="Write your task's title"
+            v-model="newTask.title"
+            :disabled="isDisibled"
+            :class="
+              newTask.title.length === 100 ? 'text-gray-500' : 'text-gray-900'
+            "
+          />
+          <p
+            v-show="newTask.title.length === 100"
+            class="pt-2 pl-4 text-xs text-gray-500 absolute"
+          >
+            The title have a maximum length of 100 characters.
+          </p>
         </div>
+        <div class="grid grid-cols-12 gap-[3vh]">
+          <div class="grid col-span-8">
+            <div>
+              <label for="description">Description</label>
+              <textarea
+                rows="15"
+                id="description"
+                class="itbkk-description block w-full p-[2vh] resize-none overflow-auto text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
+                placeholder="Write your description"
+                name="description"
+                @input="checkLength('description', newTask.description, 500)"
+                v-model="newTask.description"
+                :disabled="isDisibled"
+              ></textarea>
+            </div>
+          </div>
+
+          <div class="flex col-span-4 flex-col justify-between">
+            <div>
+              <div>
+                <label for="assignees">Assignees</label>
+                <textarea
+                  id="assignees"
+                  rows="1"
+                  class="itbkk-assignees block p-[2vh] w-full resize-none text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
+                  placeholder="Enter assignees"
+                  name="assignees"
+                  @input="checkLength('assignees', newTask.assignees, 30)"
+                  v-model="newTask.assignees"
+                  :disabled="isDisibled"
+                ></textarea>
+              </div>
+              <div class="flex flex-col pt-[3vh]">
+                <label for="status">Status</label>
+                <select
+                  id="status"
+                  class="itbkk-status select select-bordered disabled:text-white"
+                  :class="myTheme.getTheme()"
+                  :disabled="isDisibled"
+                  v-model="newTask.status"
+                >
+                  <option selected value="No Status">No Status</option>
+                  <option value="To Do">To Do</option>
+                  <option value="Doing">Doing</option>
+                  <option value="Done">Done</option>
+                </select>
+              </div>
+              <div v-if="$route.name != 'taskAdd'" class="pt-[4vh]">
+                <p class="itbkk-timezone">
+                  Local Time Zone: {{ localTimeZone }}
+                </p>
+                <p class="itbkk-created-on">Created On: {{ createdOn }}</p>
+                <p class="itbkk-updated-on">Last Updated On: {{ updatedOn }}</p>
+              </div>
+            </div>
+            <div class="pt-[4vh]">
+              <div
+                v-if="$route.name != 'taskDetails'"
+                class="flex justify-evenly"
+              >
+                <button
+                  class="itbkk-button-confirm btn btn-success btn-xs sm:btn-sm md:btn-md lg:btn-lg"
+                  @click="submitTask(true)"
+                  :disabled="newTask.title.trim().length <= 0"
+                >
+                  Ok
+                </button>
+                <button
+                  class="itbkk-button-cancel btn btn-error btn-xs sm:btn-sm md:btn-md lg:btn-lg"
+                  @click="submitTask(false)"
+                >
+                  Cancel
+                </button>
+              </div>
+              <div v-else class="flex justify-end items-end">
+                <button
+                  class="itbkk-button-close btn btn-error text-white"
+                  @click="submitTask(false)"
+                >
+                  close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped></style>
