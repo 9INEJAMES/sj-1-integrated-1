7ffab61f79dd0282163fe882508cb04b7260b2e8
yadajen <yada2342547@gmail.com>
2024-05-18 23:25:55 +0700

    fix sort

--- integrated-1-frontend/src/components/TaskTable.vue
+++ integrated-1-frontend/src/components/TaskTable.vue
@@ -58,7 +58,7 @@ const handleDeleteModal = () => {
                             class="itbkk-status-sort rounded-md w-[25px] border border-white h-[25px] relative float-right cursor-pointer shadow-md flex justify-center items-center"
                             @click="taskStore.switchSortOrder()"
                         >
-                            <img :src="`${base ? base : ''}/sort-' + taskStore.sortDirection + '.png`" class="w-[15px] h-[15px]" />
+                            <img :src="`${base ? base : ''}/sort-${taskStore.sortDirection}.png`" class="w-[15px] h-[15px]" />
                         </div>
                     </th>
                     <th style="width: 9%" class="text-center">Action</th>
