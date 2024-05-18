5cf7e2f7b720248e29c8bbe5c270519fea69b8f2
yadajen <yada2342547@gmail.com>
2024-05-18 23:06:21 +0700

    update

--- integrated-1-backend/src/main/java/int221/integrated1backend/dtos/TaskOutputAllFieldDTO.java
+++ integrated-1-backend/src/main/java/int221/integrated1backend/dtos/TaskOutputAllFieldDTO.java
@@ -2,12 +2,16 @@ package int221.integrated1backend.dtos;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class TaskOutputAllFieldDTO {
     private Integer id;
     private String title;
     private String description;
     private String assignees;
+    private Date createdOn;
+    private Date updatedOn;
     private StatusOutputDTO status;
 
 }
