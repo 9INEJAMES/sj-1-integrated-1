f9c1ae6155ca30a6bb5589946adfa1ab3fc16ffd
Niraphan <niraphan2560@gmail.com>
2024-08-17 11:43:12 +0700

    Updating on frontend side

---
+++ integrated-1-frontend/src/components/SignIn.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+import { useTheme } from '@/stores/theme.js'
+const isChecked = ref(false)
+const themeStore = useTheme()
+const base = import.meta.env.VITE_BASE
+
+</script>
+
+<template>
+    <div>
+        <div class="flex justify-center items-center h-screen">
+            <div class="w-[90vh] p-[6vh] bg-pink-300 rounded-lg shadow-lg " >
+                <h1 class="text-2xl font-bold text-center">Sign In</h1>
+                <form class="mt-4">
+                    <div class="mb-4">
+                        <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                        <input type="email" id="email" name="email" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                    </div>
+                    <div class="mb-4">
+                        <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                        <input type="password" id="password" name="password" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
--- integrated-1-frontend/src/router/index.js
+++ integrated-1-frontend/src/router/index.js
@@ -1,55 +1,61 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import TaskView from '@/views/TaskView.vue'
-import TaskModal from '@/components/TaskModal.vue'
-import StatusView from '@/views/StatusView.vue'
-import StatusModal from '@/components/StatusModal.vue'
+import { createRouter, createWebHistory } from "vue-router"
+import TaskView from "@/views/TaskView.vue"
+import TaskModal from "@/components/TaskModal.vue"
+import StatusView from "@/views/StatusView.vue"
+import StatusModal from "@/components/StatusModal.vue"
+import SignIn from "@/components/SignIn.vue"
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: '/',
-            redirect: '/task',
+            path: "/",
+            redirect: "/sign-in",
         },
         {
-            path: '/:pathMatch(.*)*',
-            redirect: '/task',
+            path: "/sign-in",
+            name: "signIn",
+            component: SignIn,
         },
         {
-            path: '/status',
-            name: 'statusView',
+            path: "/:pathMatch(.*)*",
+            redirect: "/task",
+        },
+        {
+            path: "/status",
+            name: "statusView",
             component: StatusView,
             children: [
                 {
-                    path: 'add',
-                    name: 'statusAdd',
+                    path: "add",
+                    name: "statusAdd",
                     component: StatusModal,
                 },
                 {
-                    path: ':id/edit',
-                    name: 'statusEdit',
+                    path: ":id/edit",
+                    name: "statusEdit",
                     component: StatusModal,
                 },
             ],
         },
         {
-            path: '/task',
-            name: 'taskView',
+            path: "/task",
+            name: "taskView",
             component: TaskView,
             children: [
                 {
-                    path: ':taskId',
-                    name: 'taskDetails',
+                    path: ":taskId",
+                    name: "taskDetails",
                     component: TaskModal,
                 },
                 {
-                    path: 'add',
-                    name: 'taskAdd',
+                    path: "add",
+                    name: "taskAdd",
                     component: TaskModal,
                 },
                 {
-                    path: ':taskId/edit',
-                    name: 'taskEdit',
+                    path: ":taskId/edit",
+                    name: "taskEdit",
                     component: TaskModal,
                 },
             ],
