99f09974b1490dc3c83239b0b4b02eca37f51b94
yadajen <yada2342547@gmail.com>
2024-08-20 15:23:04 +0700

    change icon position

--- integrated-1-frontend/src/components/SignIn.vue
+++ integrated-1-frontend/src/components/SignIn.vue
@@ -27,7 +27,7 @@ const loginField = ref({
                 <div class="mb-4">
                     <div class="flex justify-between items-center">
                         <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
-                        <p v-show="loginField.username.length > 0" class="block text-[0.75rem] font-medium text-gray-700">{{ loginField.username.length }} /50</p>
+                        <p v-show="loginField.username.length > 0" class="block text-[0.75rem] font-medium text-gray-500">{{ loginField.username.length }} /50</p>
                     </div>
 
                     <input
@@ -41,25 +41,25 @@ const loginField = ref({
                 </div>
                 <div class="mb-4">
                     <div class="flex justify-between items-center">
-                        <div class="flex">
-                            <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-                            <img
-                                :src="isPasswordVisible ? '../../public/eye_off_icon.png' : '../../public/eye_icon.png'"
-                                @click="togglePasswordVisibility"
-                                class="ml-2 h-5 w-5 cursor-pointercursor-pointer"
-                            />
-                        </div>
+                        <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
 
-                        <p v-show="loginField.password.length > 0" class="block text-[0.75rem] font-medium text-gray-700">{{ loginField.password.length }} /14</p>
+                        <p v-show="loginField.password.length > 0" class="block text-[0.75rem] font-medium text-gray-500">{{ loginField.password.length }} /14</p>
                     </div>
-                    <input
-                        :type="isPasswordVisible ? 'text' : 'password'"
-                        id="password"
-                        name="password"
-                        v-model="loginField.password"
-                        maxlength="14"
-                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
+                    <div class="relative">
+                        <input
+                            :type="isPasswordVisible ? 'text' : 'password'"
+                            id="password"
+                            name="password"
+                            v-model="loginField.password"
+                            maxlength="14"
+                            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm pr-10"
+                        />
+                        <img
+                            :src="isPasswordVisible ? '../../public/eye_off_icon.png' : '../../public/eye_icon.png'"
+                            @click="togglePasswordVisibility"
+                            class="absolute inset-y-2 right-3 ml-2 h-5 w-5 cursor-pointercursor-pointer"
+                        />
+                    </div>
                 </div>
                 <button :disabled="loginField.username.length <= 0 || loginField.password.length <= 0" class="itbkk-button-signin btn btn-error btn-xs sm:btn-sm md:btn-md lg:btn-lg w-full">
                     Sign In
