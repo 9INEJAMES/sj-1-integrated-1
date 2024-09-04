dbafa06981e5d84642b584a341a4b53cb6363488
Niraphan <niraphan2560@gmail.com>
2024-09-04 22:25:11 +0700

    Increase amount of commit

--- integrated-1-database/scripts/init3.0.sql
+++ integrated-1-database/scripts/init3.0.sql
@@ -6,60 +6,8 @@ USE integrated;
 SET GLOBAL time_zone = '+00:00';
 SET time_zone = '+00:00';
 
-CREATE TABLE tasks (
-    taskId INT AUTO_INCREMENT PRIMARY KEY,
-    taskTitle VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    taskDescription VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-    taskAssignees VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-    taskStatus ENUM('NO_STATUS', 'TO_DO', 'DOING', 'DONE') DEFAULT 'NO_STATUS',
-    createdOn DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updatedOn DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT CHK_taskTitle_not_empty CHECK (taskTitle <> ''),
-	CONSTRAINT CHK_taskDescription_not_empty CHECK (taskDescription <> ''),
-    CONSTRAINT CHK_taskAssignees_not_empty CHECK (taskAssignees <> '')
-)ENGINE=InnoDB;
-
-DELIMITER $$
-
-CREATE TRIGGER before_insert_tasks
-BEFORE INSERT ON tasks
-FOR EACH ROW
-BEGIN
-    SET NEW.taskTitle = TRIM(NEW.taskTitle);
-    SET NEW.taskDescription = TRIM(NEW.taskDescription);
-    SET NEW.taskAssignees = TRIM(NEW.taskAssignees);
-    SET NEW.taskStatus = UPPER(REPLACE(TRIM(NEW.taskStatus), ' ', '_'));
-    IF NEW.taskStatus IS NULL THEN
-        SET NEW.taskStatus = 'NO_STATUS';
-    END IF;
-    IF NEW.createdOn IS NULL THEN
-        SET NEW.createdOn = CURRENT_TIMESTAMP;
-    END IF;
-    IF NEW.updatedOn IS NULL THEN
-        SET NEW.updatedOn = CURRENT_TIMESTAMP;
-    END IF;
-END$$
-
-CREATE TRIGGER before_update_tasks
-BEFORE UPDATE ON tasks
-FOR EACH ROW
-BEGIN
-    SET NEW.taskTitle = TRIM(NEW.taskTitle);
-    SET NEW.taskDescription = TRIM(NEW.taskDescription);
-    SET NEW.taskAssignees = TRIM(NEW.taskAssignees);
-    SET NEW.taskStatus = UPPER(REPLACE(TRIM(NEW.taskStatus), ' ', '_'));
-    IF NEW.taskStatus IS NULL THEN
-        SET NEW.taskStatus = 'NO_STATUS';
-    END IF;
-    IF NEW.updatedOn IS NULL THEN
-        SET NEW.updatedOn = CURRENT_TIMESTAMP;
-    END IF;
-END$$
-
-DELIMITER ;
-
 CREATE TABLE boards (
-    boardId INT AUTO_INCREMENT PRIMARY KEY,
+    boardId VARCHAR(10) PRIMARY KEY,
     boardName VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
     ownerId INT,
     isLimit BOOLEAN DEFAULT FALSE,
@@ -85,7 +33,7 @@ DELIMITER ;
 
 CREATE TABLE statuses (
     statusId INT AUTO_INCREMENT,
-    boardId INT NOT NULL,
+    boardId VARCHAR(10) NOT NULL,
     statusName VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL UNIQUE,
     statusDescription VARCHAR(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
     statusColor VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT '#cbd5e1',
@@ -125,7 +73,7 @@ BEFORE DELETE ON statuses
 FOR EACH ROW
 BEGIN
 	IF OLD.statusName = 'No Status' OR OLD.statusName = 'Done' THEN
-		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot delet default status';
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Cannot delete default status';
 	END IF;
 END$$
 
@@ -133,7 +81,7 @@ DELIMITER ;
 
 CREATE TABLE tasksV2 (
     taskId INT AUTO_INCREMENT,
-    boardId INT NOT NULL,
+    boardId VARCHAR(10) NOT NULL,
     taskTitle VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
     taskDescription VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
     taskAssignees VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
@@ -218,26 +166,20 @@ END$$
 
 DELIMITER ;
 
-
+-- Sample data
 USE integrated;
 
-INSERT INTO tasks (taskTitle, taskDescription, taskAssignees, taskStatus, createdOn, updatedOn) VALUES 
-('TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0', 'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5', 'Assignees1Assignees2Assignees3', 'NO_STATUS','2024-04-22 09:00:00+00:00','2024-04-22 09:00:00+00:00'),
-('Repository', null, null, 'TO_DO', '2024-04-22 09:05:00+00:00', '2024-04-22 14:00:00+00:00'),
-('ดาต้าเบส', 'ສ້າງຖານຂໍ້ມູນ', 'あなた、彼、彼女 (私ではありません)', 'DOING', '2024-04-22 09:10:00+00:00', '2024-04-25 00:00:00+00:00'),
-('_Infrastructure_', '_Setup containers_', 'ไก่งวง กับ เพนกวิน', 'DONE','2024-04-22 09:15:00+00:00','2024-04-22 10:00:00+00:00');
-
-INSERT INTO boards (boardName, ownerId) VALUES 
-('Default', null);
+INSERT INTO boards (boardId, boardName, ownerId) VALUES 
+('1234567890', 'Default', null);
 
 INSERT INTO statuses (boardId, statusName,statusDescription,statusColor) VALUES
-(1,'No Status','The default status','#cbd5e1'),
-(1,'Done','Finished','#10b981');
+('1234567890','No Status','The default status','#cbd5e1'),
+('1234567890','Done','Finished','#10b981');
 
 INSERT INTO tasksV2 (boardId, taskTitle, statusId, createdOn, updatedOn) VALUES 
-(1,'NS01', 1,'2024-05-14 09:00:00+00:00','2024-05-14 09:00:00+00:00'),
-(1,'DN02', 2,'2024-05-14 09:50:00+00:00','2024-05-14 09:50:00+00:00');
+('1234567890','NS01', 1,'2024-05-14 09:00:00+00:00','2024-05-14 09:00:00+00:00'),
+('1234567890','DN02', 2,'2024-05-14 09:50:00+00:00','2024-05-14 09:50:00+00:00');
 
-select * from boards;
-select * from tasksV2;
-select * from statuses;
+SELECT * FROM boards;
+SELECT * FROM tasksV2;
+SELECT * FROM statuses;
