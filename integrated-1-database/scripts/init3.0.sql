a2cb8260b3416083a8af372db9ffd341d3161d5a
Natthaphol Nirattisaikul <zerozkiller@gmail.com>
2024-09-05 14:53:39 +0700

    Update init3.0.sql

--- integrated-1-database/scripts/init3.0.sql
+++ integrated-1-database/scripts/init3.0.sql
@@ -103,7 +103,7 @@ END$$
 DELIMITER ;
 
 CREATE TABLE statuses (
-    statusId INT AUTO_INCREMENT,
+    statusId INT AUTO_INCREMENT unique,
     boardId VARCHAR(10) NOT NULL,
     statusName VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL UNIQUE,
     statusDescription VARCHAR(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
