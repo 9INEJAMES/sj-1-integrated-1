31a31e227ea5c6f8f81f4451bbc2ac5a295e5f57
Natthaphol Nirattisaikul <zerozkiller@gmail.com>
2024-10-13 03:29:10 +0700

    update

--- integrated-1-frontend/src/components/CollaboratorModal.vue
+++ integrated-1-frontend/src/components/CollaboratorModal.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps, defineEmits, onMounted } from 'vue'
+import { onMounted } from 'vue'
 import { useTheme } from '@/stores/theme.js'
 import { useCollabStore } from '@/stores/collab'
 import { useAuthStore } from '@/stores/auth'
@@ -19,12 +19,12 @@ const props = defineProps({
     },
     collaborator: {
         type: Object,
-        required: false
+        required: false,
     },
     collabBoard: {
         type: Object,
-        required: false
-    }
+        required: false,
+    },
 })
 
 const emit = defineEmits(['close'])
@@ -37,9 +37,6 @@ const newCollaborator = {
     email: '',
     accessRight: 'READ',
 }
-
-
-
 
 const deleteCollaborator = async (collaboratorId) => {
     try {
@@ -67,11 +64,9 @@ const updateCollaborator = async (collaboratorId, newCol) => {
 
 const deleteCollabBoard = (collabBoard) => {
     collabStore.removeCollabBoard(collabBoard.id)
-    console.log(authStore.getAuthData().oid, collabBoard.id)
     collabApi.deleteCollabBoard(collabBoard.id, authStore.getAuthData().oid)
 
     closeModal()
-
 }
 
 const handleSubmit = async () => {
@@ -96,9 +91,8 @@ onMounted(() => {
         newCollaborator.oid = props.collaborator.oid
     }
     if (props.action === 'leaveBoard') {
-        console.log("CollabBoard to delete:", props.collabBoard)
+        console.log('CollabBoard to delete:', props.collabBoard)
     }
-
 })
 </script>
 
@@ -106,21 +100,12 @@ onMounted(() => {
     <div class="fixed inset-0 flex justify-center items-center bg-black bg-opacity-60 w-full">
         <div class="shadow-lg rounded-lg p-6 w-full max-w-xl mx-6" :class="themeStore.getTheme()">
             <div class="flex justify-between items-center mb-4">
-                <p v-if="props.action === 'add'" class="text-2xl font-semibold" :class="themeStore.getTextHeaderTheme()">Add
-                    Collaborator</p>
-                <p v-if="props.action === 'delete'" class="text-2xl font-semibold" :class="themeStore.getTextHeaderTheme()">
-                    Remove Collaborator
-                </p>
-                <p v-if="props.action === 'update'" class="text-2xl font-semibold" :class="themeStore.getTextHeaderTheme()">
-                    Change access right
-                </p>
-                <p v-if="props.action === 'leaveBoard'" class="text-2xl font-semibold" :class="themeStore.getTextHeaderTheme()">
-                    Leave Board
-                </p>
-                <button class="text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-100"
-                    @click="closeModal">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
-                        stroke="currentColor" class="w-6 h-6">
+                <p v-if="props.action === 'add'" class="text-2xl font-semibold" :class="themeStore.getTextHeaderTheme()">Add Collaborator</p>
+                <p v-if="props.action === 'delete'" class="text-2xl font-semibold" :class="themeStore.getTextHeaderTheme()">Remove Collaborator</p>
+                <p v-if="props.action === 'update'" class="text-2xl font-semibold" :class="themeStore.getTextHeaderTheme()">Change access right</p>
+                <p v-if="props.action === 'leaveBoard'" class="text-2xl font-semibold" :class="themeStore.getTextHeaderTheme()">Leave Board</p>
+                <button class="text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-100" @click="closeModal">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
@@ -144,23 +129,15 @@ onMounted(() => {
                 <div v-if="props.action === 'update'" class="font-medium">
                     Do you want to change access right of "{{ props.collaborator.name }}" to "{{ props.collaborator.accessRight === 'WRITE' ? 'READ' : 'WRITE' }}" ?
                 </div>
-                <div v-if="props.action === 'leaveBoard'" class="font-medium">
-                    Do you want to leave this "{{ props.collabBoard.name }}" board?
-                </div>
+                <div v-if="props.action === 'leaveBoard'" class="font-medium">Do you want to leave this "{{ props.collabBoard.name }}" board?</div>
             </div>
             <hr class="my-3 border-gray-300 dark:border-gray-600" />
             <div class="flex justify-end gap-2">
-                <button v-if="props.action === 'add'" @click="handleSubmit"
-                    class="px-4 py-2 text-black rounded-md bg-pink-500 ">Confirm</button>
-                <button v-if="props.action === 'delete'" @click="deleteCollaborator(props.collaborator.oid)"
-                    class="px-4 py-2 text-black rounded-md bg-pink-500 ">Delete</button>
-                <button v-if="props.action === 'update'"
-                    @click="updateCollaborator(props.collaborator.oid, newCollaborator)"
-                    class="px-4 py-2 text-black rounded-md bg-pink-500 ">Confirm</button>
-                <button v-if="props.action === 'leaveBoard'"
-                    @click="deleteCollabBoard(props.collabBoard)"
-                    class="px-4 py-2 text-black rounded-md bg-pink-500 ">Confirm</button>
-                
+                <button v-if="props.action === 'add'" @click="handleSubmit" class="px-4 py-2 text-black rounded-md bg-pink-500">Confirm</button>
+                <button v-if="props.action === 'delete'" @click="deleteCollaborator(props.collaborator.oid)" class="px-4 py-2 text-black rounded-md bg-pink-500">Delete</button>
+                <button v-if="props.action === 'update'" @click="updateCollaborator(props.collaborator.oid, newCollaborator)" class="px-4 py-2 text-black rounded-md bg-pink-500">Confirm</button>
+                <button v-if="props.action === 'leaveBoard'" @click="deleteCollabBoard(props.collabBoard)" class="px-4 py-2 text-black rounded-md bg-pink-500">Confirm</button>
+
                 <button @click="closeModal" class="px-4 py-2 text-black rounded-md bg-slate-100">Cancel</button>
             </div>
         </div>
