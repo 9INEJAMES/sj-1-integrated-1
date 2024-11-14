fda8b9975b7d7978d8db14eb5a5b0af79e2290de
yadajen <yada2342547@gmail.com>
2024-11-14 10:55:24 +0700

    update mock ui in invite collaborators page page

--- integrated-1-frontend/src/views/CollabInviteView.vue
+++ integrated-1-frontend/src/views/CollabInviteView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { useCollabApi } from '@/composables/collab-api'
 import { useCollabStore } from '@/stores/collab'
 import { useRoute, useRouter } from 'vue-router'
@@ -15,6 +15,7 @@ const collabStore = useCollabStore()
 const invite = ref({})
 const localTimeZone = ref('')
 const currentDate = ref('')
+const maxDisplayCount = 4
 
 onMounted(async () => {
     invite.value = await collabApi.getCollaboratorInvitation(route.params.bid)
@@ -46,6 +47,17 @@ const replyInvitation = async (isAccepted) => {
         })
     }
 }
+const displayedCollaborators = computed(() => {
+    return Array(Math.min(invite.value.collaborators, maxDisplayCount)).fill(null)
+})
+const getRandomInitial = () => {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return alphabet[Math.floor(Math.random() * alphabet.length)]
+}
+const randomColor = () => {
+    const colors = ['#EC4899', '#A78BFA', '#FB923C', '#F87171']
+    return colors[Math.floor(Math.random() * colors.length)]
+}
 const goBack = () => {
     // router.push({
     //     name: 'boardView',
@@ -60,7 +72,6 @@ const goBack = () => {
             <p class="text-gray-400 text-sm">{{ currentDate }}</p>
 
             <h2 class="text-3xl font-bold text-pink-500">Pending invite</h2>
-
             <div class="flex flex-col items-center space-y-2">
                 <div class="h-16 w-16 rounded-full bg-pink-500 flex items-center justify-center text-white text-2xl font-semibold">{{ invite?.ownerName?.split(' ')[1].slice(0, 1) }}</div>
                 <p class="text-xl font-semibold text-gray-800">
@@ -70,17 +81,22 @@ const goBack = () => {
                 </p>
                 <p class="text-gray-500 font-semibold">You can accept or decline this invitation.</p>
             </div>
-            <div v-if="invite.collaborators > 0">
-                <div class="flex justify-center items-center space-x-1">
-                    <div class="flex -space-x-2">
-                        <div class="h-8 w-8 rounded-full bg-pink-500 flex items-center justify-center text-white font-medium">S</div>
-                        <div class="h-8 w-8 rounded-full bg-purple-500 flex items-center justify-center text-white font-medium">M</div>
-                        <div class="h-8 w-8 rounded-full bg-orange-400 flex items-center justify-center text-white font-medium">E</div>
-                        <div class="h-8 w-8 rounded-full bg-red-400 flex items-center justify-center text-white font-medium">A</div>
+            <div>
+                <div class=" items-center space-x-1">
+                    <div class="flex justify-center pb-3 -space-x-2">
+                        <div
+                            v-for="(collaborator, index) in displayedCollaborators"
+                            :key="index"
+                            class="h-8 w-8 rounded-full flex items-center justify-center text-white font-medium"
+                            :style="{ backgroundColor: randomColor() }"
+                        >
+                            {{ getRandomInitial() }}
+                        </div>
+
+                        <span v-if="invite.collaborators > maxDisplayCount" class="text-gray-500 text-sm"> +{{ invite.collaborators - maxDisplayCount }} </span>
                     </div>
-                    <span class="text-gray-500 text-sm">+5</span>
+                    <p class="text-gray-500 text-sm">{{ invite.collaborators }} members have already become collaborators on this board.</p>
                 </div>
-                <p class="text-gray-500 text-sm">{{ invite.collaborators }} members have already become collaborators on this board.</p>
             </div>
             <div class="flex justify-center space-x-4 mt-4">
                 <button class="w-1/3 py-2 rounded-lg border border-gray-300 text-gray-500 font-semibold hover:bg-gray-100" @click="replyInvitation(false)">Decline</button>
@@ -99,7 +115,7 @@ const goBack = () => {
             <p class="text-gray-600 text-lg">It looks like the invitation link might be outdated or invalid.</p>
             <p class="text-gray-600">If you believe this is an error, please check the link or contact the board owner to request a new invitation.</p>
 
-            <div class=" flex justify-center">
+            <div class="flex justify-center">
                 <button @click="goBack" class="mt-4 bg-red-400 text-white px-6 py-3 rounded-full flex items-center justify-center hover:bg-red-500 transition duration-200 ease-in-out">
                     <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
